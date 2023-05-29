3bd98059ff4b33496a654817e0e353d250095e57
Laxcod <90342920+Laxcod@users.noreply.github.com>
2023-05-29 18:13:27 +0700

    add 3 enemies
    menambahkan 3 musuh pada percobaan game ini

--- Assets/Scripts/Enemy Script/EnemyMovements.cs
+++ Assets/Scripts/Enemy Script/EnemyMovements.cs
@@ -11,16 +11,25 @@ public class EnemyMovements : MonoBehaviour
 
     public bool isStatic;
     public bool isWalker;
+    public bool isPatrol;
+    public bool shoudlWait;
+    public float timeToWait;
+    public bool isWaiting;
     public bool walksRight;
 
     public Transform wallCheck, pitCheck, groundCheck;
-    public bool walldetected, pitDetected, isGround;
+    bool walldetected, pitDetected, isGrounded;
     public float detectionRadius;
     public LayerMask whatIsGround;
+
+    public Transform pointA, pointB;
+    bool goToA, goToB;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        goToA = true;
         speed = GetComponent<Enemy>().speed;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
@@ -31,8 +40,9 @@ public class EnemyMovements : MonoBehaviour
     {
         pitDetected = !Physics2D.OverlapCircle(pitCheck.position, detectionRadius,whatIsGround);
         walldetected = Physics2D.OverlapCircle(wallCheck.position, detectionRadius,whatIsGround);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position,detectionRadius, whatIsGround);
 
-        if(pitDetected || walldetected)
+        if( (pitDetected || walldetected) && isGrounded  )
         {
             Flip();
         }
@@ -48,6 +58,7 @@ public class EnemyMovements : MonoBehaviour
         if(isWalker)
         {
             rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            anim.SetBool("Idle", false);
 
             if(!walksRight)
             {
@@ -58,6 +69,65 @@ public class EnemyMovements : MonoBehaviour
                 rb.velocity = new Vector2(speed * Time.deltaTime, rb.velocity.y);
             }
         }
+        if(isPatrol)
+        {
+            
+            if(goToA)
+            {
+                if(!isWaiting)
+                {
+                    anim.SetBool("Idle", false);
+                    rb.velocity = new Vector2 (-speed * Time.deltaTime, rb.velocity.y);
+                }
+
+                
+
+                if(Vector2.Distance(transform.position,  pointA.position)< 0.2f)
+                {
+                    
+                    if(shoudlWait)
+                    {
+                       StartCoroutine(Waiting());   
+                    }
+
+                    Flip();
+                    goToA = false;
+                    goToB = true;
+                }
+            }
+
+            if(goToB)
+            {
+                if(!isWaiting)
+                {
+                    anim.SetBool("Idle", false);
+                    rb.velocity = new Vector2 (speed * Time.deltaTime, rb.velocity.y);
+                }
+
+                if(Vector2.Distance(transform.position, pointB.position) < 0.2f)
+                {
+                    if(shoudlWait)
+                    {
+                        StartCoroutine(Waiting()); 
+                    }
+
+                    Flip();
+                    goToA = true;
+                    goToB = false;
+                }
+            }
+        }
+    }
+
+    IEnumerator Waiting()
+    {
+        anim.SetBool("Idle", true);
+        isWaiting = true;
+        Flip();
+        yield return new WaitForSeconds(timeToWait);
+        isWaiting = false;
+        anim.SetBool("Idle", false);
+        Flip();
     }
 
 
