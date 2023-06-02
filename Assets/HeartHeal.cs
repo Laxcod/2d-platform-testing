202a662d435fa2ae9ffde54d93270dc9d4e4d158
Laxcod <90342920+Laxcod@users.noreply.github.com>
2023-06-02 11:20:08 +0700

    Add Testing Heart System
    menambahkan percobaan heart system

---
+++ Assets/HeartHeal.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartHeal : MonoBehaviour
+{
+
+    [SerializeField] private int healAmount;
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        // Player player = collider.GetComponent<Player>();
+        // if (player != null) {
+            // We hit the Player
+            // player.Heal(healAmount);
+            Destroy(gameObject);
+        }
+    }
