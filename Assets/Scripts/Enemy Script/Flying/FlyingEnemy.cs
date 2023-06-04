b93cd54ddba8b53594828d005ecba66eb1a2923f
Nazhif Azka Fadlillah <nazhif245@gmail.com>
2023-06-04 20:24:48 +0700

    Add Proto Flying Enemy

---
+++ Assets/Scripts/Enemy Script/Flying/FlyingEnemy.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
+{
+    [SerializeField] float speed;
+    public bool chase = false;
+    public Transform startingPoint;
+    private GameObject player;
+    
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (player == null)
+        return;;
+        if (chase == true)
+            Chase();
+        else
+            ReturnStart();
+            Flip();
+    }
+
+    private void Chase()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        if(Vector2.Distance(transform.position, player.transform.position) <= 0.5f)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    private void ReturnStart()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, startingPoint.position, speed * Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+        if (transform.position.x > player.transform.position.x)
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        else
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+    }
+
+}
