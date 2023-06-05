b361396ad3c9d85cd2e35fd2231325a9356de4b5
Laxcod <90342920+Laxcod@users.noreply.github.com>
2023-06-05 23:39:27 +0700

    Add Ground Check And Jump Animation
    Menambahkan Ground Check Dan Animasi Loncat

--- Assets/PlayerControllers.cs
+++ Assets/PlayerControllers.cs
@@ -7,17 +7,32 @@ public class PlayerControllers : MonoBehaviour
 
     private float movementInputDirection;
 
+    private int amountOfJumpsLeft;
+
     private bool isFacingRight = true;
+    private bool isWalking;
+    private bool isGrounded;
+    private bool canJump;
 
     private Rigidbody2D rb;
+    private Animator anim;
+
+    private int amountOfJumps = 1;
 
     public float movementSpeed = 10.0f;
     public float jumpForce = 16.0f;
+    public float groundCheckRadius;
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
     }
 
     // Update is called once per frame
@@ -25,11 +40,36 @@ public class PlayerControllers : MonoBehaviour
     {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate() 
     {
         ApplyMovement();
+        CheckSurroundings();
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void CheckIfCanJump()
+    {
+        if(isGrounded && rb.velocity.y <= 0)
+        {
+            amountOfJumpsLeft = amountOfJumps;
+        }
+      
+        if(amountOfJumpsLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
     }
 
     private void CheckMovementDirection()
@@ -41,6 +81,15 @@ public class PlayerControllers : MonoBehaviour
         else if (!isFacingRight && movementInputDirection > 0 )
         {
             Flip();
+        }
+
+        if(rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
         }
     }
     private void CheckInput()
@@ -55,9 +104,20 @@ public class PlayerControllers : MonoBehaviour
 
     }
 
+    private void UpdateAnimations()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+    }
+
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+        }
     }
 
     private void ApplyMovement()
@@ -69,5 +129,10 @@ public class PlayerControllers : MonoBehaviour
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void OnDrawGizmos() 
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
