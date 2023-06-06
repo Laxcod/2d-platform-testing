4f293b68b773058f0b4d5097badfe860a24a2359
Laxcod <90342920+Laxcod@users.noreply.github.com>
2023-06-06 15:46:03 +0700

    Add Wall Jump (still bug)
    Menambahkan wall Jump

--- Assets/PlayerControllers.cs
+++ Assets/PlayerControllers.cs
@@ -12,6 +12,8 @@ public class PlayerControllers : MonoBehaviour
     private bool isFacingRight = true;
     private bool isWalking;
     private bool isGrounded;
+    private bool isTouchingWall;
+    private bool isWallSliding;
     private bool canJump;
 
     private Rigidbody2D rb;
@@ -22,8 +24,14 @@ public class PlayerControllers : MonoBehaviour
     public float movementSpeed = 10.0f;
     public float jumpForce = 16.0f;
     public float groundCheckRadius;
+    public float wallcheckDistance;
+    public float wallSlideSpeed;
+    public float movementForceInAir;
+    public float airDragMultiplier = 0.95f;
+    public float variableJumpheightMultiplier = 0.5f;
 
     public Transform groundCheck;
+    public Transform wallcheck;
 
     public LayerMask whatIsGround;
 
@@ -42,6 +50,7 @@ public class PlayerControllers : MonoBehaviour
         CheckMovementDirection();
         UpdateAnimations();
         CheckIfCanJump();
+        CheckIfWallSliding();
     }
 
     private void FixedUpdate() 
@@ -50,9 +59,23 @@ public class PlayerControllers : MonoBehaviour
         CheckSurroundings();
     }
 
+    private void CheckIfWallSliding()
+    {
+        if(isTouchingWall && !isGrounded && rb.velocity.y < 0)
+        {
+            isWallSliding = true;
+        }
+        else
+        {
+            isWalking  = false;
+        }
+    }
+
     private void CheckSurroundings()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        isTouchingWall = Physics2D.Raycast(wallcheck.position, transform.right, wallcheckDistance, whatIsGround);
     }
 
     private void CheckIfCanJump()
@@ -102,6 +125,10 @@ public class PlayerControllers : MonoBehaviour
             Jump();
         }
 
+    //   is(Input.GetButton("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpheightMultiplier);
+        }
     }
 
     private void UpdateAnimations()
@@ -109,6 +136,8 @@ public class PlayerControllers : MonoBehaviour
         anim.SetBool("isWalking", isWalking);
         anim.SetBool("isGrounded", isGrounded);
         anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("isWallSliding", isWallSliding);
+
     }
 
     private void Jump()
@@ -122,17 +151,48 @@ public class PlayerControllers : MonoBehaviour
 
     private void ApplyMovement()
     {
-        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+
+        if(isGrounded)
+        {
+            rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+        }
+        else if(!isGrounded && !isWallSliding && movementInputDirection !=0 )
+        {
+            Vector2 forceToAdd = new Vector2(movementForceInAir * movementInputDirection, 0);
+            rb.AddForce(forceToAdd);
+
+            if(Mathf.Abs(rb.velocity.x) > movementSpeed)
+            {
+                rb.velocity = new Vector2 (movementSpeed * movementInputDirection, rb.velocity.y);
+            }
+        }
+        else if(!isGrounded && !isWallSliding && movementInputDirection == 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x * airDragMultiplier, rb.velocity.y);
+        }
+        
+        if(isWallSliding)  
+        {
+            if(rb.velocity.y < -wallSlideSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -wallSlideSpeed);
+            }
+        }      
     }
 
     private void Flip()
     {
+        if(!isWallSliding)
+        {
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
+        }
     }
 
     private void OnDrawGizmos() 
     {
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+
+        Gizmos.DrawLine(wallcheck.position, new Vector3(wallcheck.position.x + wallcheckDistance, wallcheck.position.y, wallcheck.position.z));
     }
 }
