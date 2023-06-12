636e6be472f85ade0382dd38acf638eed7042cc4
Nazhif Azka Fadlillah <nazhif245@gmail.com>
2023-06-12 20:09:37 +0700

    Edit UI

--- Assets/Scripts/SceneScripts/ParallaxBackground.cs
+++ Assets/Scripts/SceneScripts/ParallaxBackground.cs
@@ -5,36 +5,69 @@ using UnityEngine;
 public class ParallaxBackground : MonoBehaviour
 {
     public ParallaxCamera parallaxCamera;
-    List<ParallaxLayer> parallaxLayers = new List<ParallaxLayer>();
+    private Transform playerTransform;
+    private Vector3 lastPlayerPosition;
+    private Vector3 deltaPlayerPosition;
 
-    void Start()
+    private void Start()
     {
         if (parallaxCamera == null)
             parallaxCamera = Camera.main.GetComponent<ParallaxCamera>();
+
         if (parallaxCamera != null)
             parallaxCamera.onCameraTranslate += Move;
-        SetLayers();
+
+        // Find the player's transform (replace "Player" with your actual player object name or tag)
+        playerTransform = GameObject.FindWithTag("Player").transform;
+        lastPlayerPosition = playerTransform.position;
     }
 
-    void SetLayers()
+    private void Move(float delta)
     {
-        parallaxLayers.Clear();
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            ParallaxLayer layer = transform.GetChild(i).GetComponent<ParallaxLayer>();
+        // Calculate the movement of the player since the last frame
+        deltaPlayerPosition = playerTransform.position - lastPlayerPosition;
 
-            if (layer != null)
-            {
-                layer.name = "Layer-" + i;
-                parallaxLayers.Add(layer);
-            }
-        }
-    }
-    void Move(float delta)
-    {
-        foreach (ParallaxLayer layer in parallaxLayers)
+        foreach (ParallaxLayer layer in transform.GetComponentsInChildren<ParallaxLayer>())
         {
-            layer.Move(delta);
+            layer.Move(delta, deltaPlayerPosition);
         }
+
+        lastPlayerPosition = playerTransform.position;
     }
+
+
+
+    //public ParallaxCamera parallaxCamera;
+    //List<ParallaxLayer> parallaxLayers = new List<ParallaxLayer>();
+
+    //void Start()
+    //{
+    //    if (parallaxCamera == null)
+    //        parallaxCamera = Camera.main.GetComponent<ParallaxCamera>();
+    //    if (parallaxCamera != null)
+    //        parallaxCamera.onCameraTranslate += Move;
+    //    SetLayers();
+    //}
+
+    //void SetLayers()
+    //{
+    //    parallaxLayers.Clear();
+    //    for (int i = 0; i < transform.childCount; i++)
+    //    {
+    //        ParallaxLayer layer = transform.GetChild(i).GetComponent<ParallaxLayer>();
+
+    //        if (layer != null)
+    //        {
+    //            layer.name = "Layer-" + i;
+    //            parallaxLayers.Add(layer);
+    //        }
+    //    }
+    //}
+    //void Move(float delta)
+    //{
+    //    foreach (ParallaxLayer layer in parallaxLayers)
+    //    {
+    //        layer.Move(delta);
+    //    }
+    //}
 }
