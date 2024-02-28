ed0ac03633b61962d83af75838e68d1c93ede67d
furukawaa <eszanuria15@gmail.com>
2024-02-29 00:34:37 +0800

    lvl 4 100% + metroidvania 50%

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
     public void Play()
     {
         SceneManager.LoadScene("LevelSelector");
+        // SceneManager.LoadScene("Gameplay");
     }
 
     public void Setting()
